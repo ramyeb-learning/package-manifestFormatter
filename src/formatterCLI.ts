c79b43cea9051cacc16f8e14dfbb46674ccd2c08
RamyEB <ramy.elb@gmail.com>
2022-10-13 17:56:35 +0200

    verify maniufest's directory

--- src/formatterCLI.ts
+++ src/formatterCLI.ts
@@ -2,10 +2,27 @@ import * as fs from 'fs';
 import path from 'path';
 import {verifyManifest} from "./formatter"
 
-if(process.argv[2] != undefined){
-    const manifest = JSON.parse(fs.readFileSync(path.join(process.cwd(),process.argv[2]), 'utf-8'));
-    let details = process.argv.includes('-details')
-    let steps = process.argv.includes('-steps')
-    let disableState = process.argv.includes('-disableState')
-    verifyManifest(manifest, {details, steps, disableState})
+const verifyManifestFromCLI = (): void | never => {
+    try {
+    if(process.argv[2] != undefined){
+
+        let details = process.argv.includes('-details')
+        let steps = process.argv.includes('-steps')
+        let disableState = process.argv.includes('-disableState')
+        fs.readdir(process.argv[2], (err, files) => {
+            files.forEach(file => {
+                console.log("------"+file+"------")
+               if(!verifyManifest(JSON.parse(fs.readFileSync(path.join(process.cwd(),process.argv[2],file), 'utf-8')), {details, steps, disableState})){
+                throw new Error("The JSON file : " + file +" do not correspond to the schema");
+               }
+            })
+          })
+    }else{
+        throw new Error("Please specify the manifests directory");
+    }
+    }catch(e){
+        console.log(e) 
+    }
 }
+
+verifyManifestFromCLI();
